b4a76dd79744cc7e5781887b44c1b2124d718dbc
Aditya Gupta <ashishgupta9936ad@gmail.com>
2023-11-22 02:00:17 +0530

    design: design splash screen

---
+++ app/src/main/java/com/sparklead/newsnow/ui/fragment/SplashScreenFragment.kt
@@ -0,0 +1,36 @@
+package com.sparklead.newsnow.ui.fragment
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.sparklead.newsnow.R
+import com.sparklead.newsnow.databinding.FragmentSplashScreenBinding
+
+class SplashScreenFragment : Fragment() {
+
+    private var _binding: FragmentSplashScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashScreenFragment_to_homeFragment)
+        }, 1500)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
